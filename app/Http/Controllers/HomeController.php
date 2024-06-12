5d4e6e1eac5be8d48e48cc84810f86a796f4f89e
Harshit <hc.web1992@gmail.com>
2024-06-12 12:55:05 +0530

    Increase Number of books to display on the homepage -- Previous Was 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     // It will display homepage with all Books
     public function allBooks(){
-        $books = Book::paginate(4);
+        $books = Book::paginate(8);
         return view('home.books', compact('books'));
     }
 
